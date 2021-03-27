bdd081b4c573d6f507abf689b52771c1b2818ffc
xyl <416319808@qq.com>
2021-03-27 22:53:03 +0800

    upload

--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"grpc-client/pb"
 	"io"
 	"log"
+	"os"
+	"time"
 )
 
 const (
-	port=":9000"
+	port = ":9000"
 )
+
 /*
 func main() {
 	creds,err:=credentials.NewClientTLSFromFile("server.pem", "")
@@ -28,23 +32,24 @@ func main() {
 	client:=pb.NewEmployeeServiceClient(conn)
 	GetByNo(client)
 }
- */
+*/
 
 func main() {
-	conn,err:=grpc.Dial("127.0.0.1"+port,grpc.WithInsecure())
-	if err!=nil {
+	conn, err := grpc.Dial("127.0.0.1"+port, grpc.WithInsecure())
+	if err != nil {
 		log.Fatalln(err.Error())
 	}
 	defer conn.Close()
 
-	client :=pb.NewEmployeeServiceClient(conn)
+	client := pb.NewEmployeeServiceClient(conn)
 	//GetByNo(client)
-	GetAll(client)
+	//GetAll(client)
+	addPhoto(client)
 }
 
-func GetByNo(client pb.EmployeeServiceClient)  {
-	res,err:=client.GetByNo(context.Background(), &pb.GetByNoRequest{No: 1990})
-	if err!=nil {
+func GetByNo(client pb.EmployeeServiceClient) {
+	res, err := client.GetByNo(context.Background(), &pb.GetByNoRequest{No: 1990})
+	if err != nil {
 		log.Fatalln(err.Error())
 	}
 
@@ -53,21 +58,62 @@ func GetByNo(client pb.EmployeeServiceClient)  {
 	fmt.Println(res.Employee.Id)
 }
 
-func GetAll(client pb.EmployeeServiceClient)  {
-	stream,err:=client.GetAll(context.Background(),&pb.GetAllRequest{})
-	if err!=nil {
+func GetAll(client pb.EmployeeServiceClient) {
+	stream, err := client.GetAll(context.Background(), &pb.GetAllRequest{})
+	if err != nil {
 		log.Fatalln(err.Error())
 	}
 
-	for  {
-		res,err:=stream.Recv()
-		if err==io.EOF {
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
 			break
 		}
-		if err!=nil {
+		if err != nil {
 			log.Fatalln(err.Error())
 		}
 		fmt.Println(res.Employee)
 	}
 }
 
+func addPhoto(client pb.EmployeeServiceClient) {
+	imgFile, err := os.Open("beauty.jpg")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer imgFile.Close()
+
+	md := metadata.New(map[string]string{
+		"no": "2004",
+	})
+	ctx := context.Background()
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	// 传数据
+	stream, err := client.AddPhoto(ctx)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	for {
+		chunk := make([]byte, 1024*128)
+		chunkSize, err := imgFile.Read(chunk)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		// 读取到最后一块
+		if chunkSize < len(chunk) {
+			chunk = chunk[:chunkSize]
+		}
+		stream.Send(&pb.AddPhotoRequest{Data: chunk})
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	fmt.Println(res.Ok)
+}
